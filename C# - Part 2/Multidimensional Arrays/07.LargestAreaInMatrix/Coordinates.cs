3609f57718df61a46b47f9831236f44c060606b4
BiserSirakov <biser.sirakov@gmail.com>
2015-02-18 18:33:33 +0200

    Added Multidimensional Arrays

---
+++ C# - Part 2/Multidimensional Arrays/07.LargestAreaInMatrix/Coordinates.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Coordinates
+{
+    private int x;
+    private int y;
+
+    public int X
+    {
+        get
+        {
+            return this.x;
+        }
+        private set
+        {
+            this.x = value;
+        }
+    }
+
+    public int Y
+    {
+        get
+        {
+            return this.y;
+        }
+        private set
+        {
+            this.y = value;
+        }
+    }
+
+    public Coordinates(int firstDimension, int secondDimension)
+    {
+        this.X = firstDimension;
+        this.Y = secondDimension;
+    }
+}
